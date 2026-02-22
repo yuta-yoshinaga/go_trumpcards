d3493f51c5bf6ff38946de96d9112ecb3cc05908
yuta-yoshinaga <yuta.yoshinaga@gmail.com>
2026-02-22 21:48:08 +0900

    Revert "refactor(entities): compute BlackJackPlayer score on-demand in GetScore"
    This reverts commit bba5fedae30dc555d781c7bb3b49b65902b3e6e6.

--- entities/BlackJackPlayer.go
+++ entities/BlackJackPlayer.go
@@ -2,7 +2,8 @@ package entities
 
 // BlackJackPlayer ブラックジャックプレイヤークラス
 type BlackJackPlayer struct {
-	Player // 親クラス
+	Player     // 親クラス
+	score  int // スコア
 }
 
 // NewBlackJackPlayer コンストラクタ
@@ -11,30 +12,32 @@ func NewBlackJackPlayer() *BlackJackPlayer {
 		Player: Player{
 			cards: make([]*Card, 0),
 		},
+		score: 0,
 	}
 }
 
 // AddCard カード追加
 func (bp *BlackJackPlayer) AddCard(card *Card) {
 	bp.cards = append(bp.cards, card)
+	bp.CalcScore()
 }
 
-// GetScore 手札から現在のスコア計算
-func (bp *BlackJackPlayer) GetScore() int {
+// CalcScore 手札から現在のスコア計算
+func (bp *BlackJackPlayer) CalcScore() {
 	aceFlag := false
-	score := 0
+	bp.score = 0
 	for i := 0; i < len(bp.cards); i++ {
 		value := bp.cards[i].GetValue()
 		if 2 <= value && value <= 10 {
 			// 2～10
-			score += value
+			bp.score += value
 		} else if 11 <= value && value <= 13 {
 			// 11～13
-			score += 10
+			bp.score += 10
 		} else {
 			if aceFlag {
 				// 2枚目のエースは強制的に1で換算する
-				score++
+				bp.score++
 			} else {
 				// エースは後ほど計算する
 				aceFlag = true
@@ -43,18 +46,22 @@ func (bp *BlackJackPlayer) GetScore() int {
 	}
 	if aceFlag {
 		// エース計算
-		tmpScore1 := score + 1
-		tmpScore2 := score + 11
+		tmpScore1 := bp.score + 1
+		tmpScore2 := bp.score + 11
 		if 22 <= tmpScore1 && 22 <= tmpScore2 {
 			// どちらもバーストしているならエースを1
-			score = tmpScore1
+			bp.score = tmpScore1
 		} else if tmpScore1 <= 21 && 22 <= tmpScore2 {
 			// エースが11でバーストしているならエースを1
-			score = tmpScore1
+			bp.score = tmpScore1
 		} else {
 			// どちらもバーストしていないならエースを11
-			score = tmpScore2
+			bp.score = tmpScore2
 		}
 	}
-	return score
+}
+
+// GetScore スコア取得
+func (bp *BlackJackPlayer) GetScore() int {
+	return bp.score
 }
