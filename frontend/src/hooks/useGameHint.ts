81ba9f44b389a2820a22591e8836494ba822796f
吉永祐太 <yuta.yoshinaga@gmail.com>
2026-08-02 17:03:51 +0900

    feat(hint): add the Pai Gow Poker hint (#4557)

--- frontend/src/hooks/useGameHint.ts
+++ frontend/src/hooks/useGameHint.ts
@@ -134,6 +134,7 @@ import type {
   OpenFaceChineseResponse,
   OsmosisResponse,
   PageOneResponse,
+  PaiGowResponse,
   PanResponse,
   PenguinResponse,
   PigsTailResponse,
@@ -367,6 +368,7 @@ import { getOmbreHint } from '../utils/hints/ombreHint';
 import { getOpenFaceChineseHint } from '../utils/hints/openfacechineseHint';
 import { getOsmosisHint } from '../utils/hints/osmosisHint';
 import { getPageOneHint } from '../utils/hints/pageoneHint';
+import { getPaiGowHint } from '../utils/hints/paigowHint';
 import { getPanHint } from '../utils/hints/panHint';
 import { getPenguinHint } from '../utils/hints/penguinHint';
 import { getPigstailHint } from '../utils/hints/pigstailHint';
@@ -581,7 +583,7 @@ export const hintFactories = {
   thirtyone: (s) => getThirtyOneHint(s as ThirtyOneResponse),
   yaniv: (s) => getYanivHint(s as YanivResponse),
   tressette: (s) => getTressetteHint(s as TressetteResponse),
-  paigow: () => null,
+  paigow: (s) => getPaiGowHint(s as PaiGowResponse),
   chinesepoker: () => null,
   pageone: (s) => getPageOneHint(s as PageOneResponse),
   pigtail: (s) => getPigstailHint(s as PigsTailResponse),
