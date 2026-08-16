c1426d7d34fe020922b4abe2b425f3b704d83461
t <t@t>
2026-08-17 00:24:52 +0900

    style(missmilligan): group the project imports separately
    golangci-lint の goimports は local-prefixes を設定しているので、
    自プロジェクトの import は third-party と別グループにする必要がある。

--- internal/adapter/presenter/MissMilliganError_test.go
+++ internal/adapter/presenter/MissMilliganError_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
 	"github.com/yuta-yoshinaga/go_trumpcards/internal/domain"
 	"github.com/yuta-yoshinaga/go_trumpcards/internal/i18n"
 )
